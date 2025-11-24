739ee7257fe23fa746d7f4774e84ca1502a4ea2d
ggpillow <ketnut3336@gmail.com>
2025-11-25 03:54:56 +1100

    push changes in bransh main

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 spb_price = 50_000
 msc_price = 80_000
 ekb_price = 40_000
+
+city = input("Введите город, в который хотите поехать: ")
+count = int(input("Введите количество туристов: "))
+
+total = 0
+
+if city == "spb":
+    total = spb_price * count + spb_price * count / 2
+elif city == "msc":
+    total = msc_price * count + msc_price * count / 2
+elif city == "ekb":
+    total = ekb_price * count + ekb_price * count / 2
+else:
+    print("Введен город, которого нет!")
+    exit()
+
+print("Итоговый бюджет: " + str(total))
