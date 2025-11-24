24a3f05903755fc1a6c8bca3b64bc1327766b371
ggpillow <ketnut3336@gmail.com>
2025-11-25 04:08:27 +1100

    changes main

--- main.py
+++ main.py
@@ -5,22 +5,6 @@ ekb_price = 40_000
 print("Приветствую в системе 'Помощь туриста'")
 print("Доступны следующие города для расчёта стоимости билетов: spb, msc, ekb")
 print("-----------------------------------")
-city = input("Введите город, в который хотите поехать: ")
-count = int(input("Введите количество туристов: "))
-
-total = 0
-
-if city == "spb":
-    total = spb_price * count + spb_price * count / 2
-elif city == "msc":
-    total = msc_price * count + msc_price * count / 2
-elif city == "ekb":
-    total = ekb_price * count + ekb_price * count / 2
-else:
-    print("Введен город, которого нет!")
-    exit()
-
-print("Итоговый бюджет: " + str(total))
 
 city = input("Введите город, в который хотите поехать: ")
 parents_count = int(input("Введите количество туристов: "))
